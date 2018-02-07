77ea583f287d1b3f751f90525a28bf7a0058c5aa
Ariel Junior <arieljuniorc@outlook.com>
2018-02-07 13:17:17 -0200

    alteração na tag img do produto

--- src/view_product/view_product.jsx
+++ src/view_product/view_product.jsx
@@ -5,7 +5,7 @@ export default props =>{
     
     return (
     <figure className="figure">
-        <img src={props.path} className="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure." title={props.title}/>
+        <img src={props.path} className="figure-img img-fluid rounded" title={props.title}/>
     </figure>
     )
 }
